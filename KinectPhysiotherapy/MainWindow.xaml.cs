e464fb3cfb3e381337e6f7636ee8728b132d90f1
Piotr Stus <Piters2s@gmail.com>
2018-02-27 21:56:40 +0100

    Start

--- KinectPhysiotherapy/MainWindow.xaml.cs
+++ KinectPhysiotherapy/MainWindow.xaml.cs
@@ -12,17 +12,68 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Kinect;
+using System.ComponentModel;
 
 namespace KinectPhysiotherapy
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        private KinectSensor kinect = null;
+        private string statusText = null;
+
         public MainWindow()
         {
+            this.kinect = KinectSensor.GetDefault();
+            this.kinect.IsAvailableChanged += this.Sensor_IsAvailableChanged;
+            this.StatusText = this.kinect.IsAvailable ? "Kinect available." : " Kinect is Unavailable"; 
+            this.DataContext = this;
+
             InitializeComponent();
+
+            this.kinect.Open();
         }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Sensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs e)
+        {
+            this.StatusText = this.kinect.IsAvailable ? "Kinect available." : "Goodbie World! Kinect is Unavailable!";
+        }
+
+        public string StatusText
+        {
+            get
+            {
+                return this.statusText;
+            }
+
+            set
+            {
+                if (this.statusText != value)
+                {
+                    this.statusText = value;
+
+                    if (this.PropertyChanged != null)
+                    {
+                        this.PropertyChanged(this, new PropertyChangedEventArgs("StatusText"));
+                    }
+                }
+            }
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            if (this.kinect != null) 
+            {
+                this.kinect.Close();
+                this.kinect = null;
+            }
+        }
+
+
     }
 }
