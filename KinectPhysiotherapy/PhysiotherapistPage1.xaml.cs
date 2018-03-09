471b6596cd28a0ef04c8f9e61e236e20e939ceb7
Piotr Stus <Piters2s@gmail.com>
2018-03-10 00:05:05 +0100

    Drawing ver. 2

--- KinectPhysiotherapy/PhysiotherapistPage1.xaml.cs
+++ KinectPhysiotherapy/PhysiotherapistPage1.xaml.cs
@@ -14,16 +14,23 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.Kinect;
 
+
+
+
 namespace KinectPhysiotherapy
 {
     /// <summary>
     /// Interaction logic for PhysiotherapistPage1.xaml
     /// </summary>
+    /// 
+   
     public partial class PhysiotherapistPage1 : Page
     {
+        
         KinectSensor _sensor;
         MultiSourceFrameReader _reader;
-        IList<Body> _bodies;
+        //IList<Body> _bodies; // body ver 1
+        Body[] bodies; //body ver 2
         bool _displayBody = false;
 
         public PhysiotherapistPage1()
@@ -31,6 +38,7 @@ namespace KinectPhysiotherapy
             InitializeComponent();
         }
 
+        //Start recording button
         private void StartRecording(object sender, RoutedEventArgs e)
         {
             _sensor = KinectSensor.GetDefault();
@@ -39,6 +47,7 @@ namespace KinectPhysiotherapy
             {
                 _sensor.Open();
 
+                bodies = new Body[6]; //Body ver 2
                 _reader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Body);
                
                 _reader.MultiSourceFrameArrived += Reader_MultiSourceFrameArrived;
@@ -51,45 +60,79 @@ namespace KinectPhysiotherapy
         {
             // Get a reference to the multi-frame
             var reference = e.FrameReference.AcquireFrame();
-
-            // Open color frame
-            using (var frame = reference.ColorFrameReference.AcquireFrame())
+            //Body ver 2 reference
+            //var bodyFrame = reference.BodyFrameReference.AcquireFrame();
+            using (BodyFrame bodyFrame = reference.BodyFrameReference.AcquireFrame())
             {
-                if (frame != null)
+                // Open color frame
+                using (var frame = reference.ColorFrameReference.AcquireFrame())
                 {
-                   
-                        camera.Source = ToBitmap(frame);
-                   
-                }
-            }
-
-            // Body
-            using (var frame = reference.BodyFrameReference.AcquireFrame())
-            {
-                if (frame != null)
-                {
-                    canvas.Children.Clear();
-
-                    _bodies = new Body[frame.BodyFrameSource.BodyCount];
-
-                    frame.GetAndRefreshBodyData(_bodies);
-
-                    foreach (var body in _bodies)
+                    if (frame != null && bodyFrame != null)
                     {
-                        if (body != null)
+
+                        camera.Source = ToBitmap(frame);
+
+                        //Body ver 2
+                        bodyFrame.GetAndRefreshBodyData(bodies);
+                        bodyCanvas.Children.Clear();
+
+                        foreach (var body in bodies)
                         {
                             if (body.IsTracked)
                             {
-                                // Draw skeleton.
-                                if (_displayBody)
+
+                                Joint leftHandJoint = body.Joints[JointType.HandLeft];
+                                if (leftHandJoint.TrackingState == TrackingState.Tracked)
                                 {
-                                    canvas.DrawSkeleton(body);
+                                    DepthSpacePoint dsp = _sensor.CoordinateMapper.MapCameraPointToDepthSpace(leftHandJoint.Position);
+
+
+                                    Ellipse LeftHandCircle = new Ellipse() { Width = 50, Height = 50, Fill = new SolidColorBrush(Color.FromArgb(255, 255, 0, 0)) };
+
+
+                                    bodyCanvas.Children.Add(LeftHandCircle);
+
+
+                                    Canvas.SetLeft(LeftHandCircle, dsp.X);
+                                    Canvas.SetTop(LeftHandCircle, dsp.Y - 100);
                                 }
                             }
                         }
+
                     }
                 }
             }
+           
+
+            
+
+            // Body
+            //using (var frame = reference.BodyFrameReference.AcquireFrame())
+            //{
+            //    if (frame != null)
+            //    {
+            //        canvas.Children.Clear();
+
+            //        _bodies = new Body[frame.BodyFrameSource.BodyCount];
+
+            //        frame.GetAndRefreshBodyData(_bodies);
+
+            //        foreach (var body in _bodies)
+            //        {
+            //            if (body != null)
+            //            {
+            //                if (body.IsTracked)
+            //                {
+            //                    // Draw skeleton.
+            //                    if (_displayBody)
+            //                    {
+            //                        canvas.DrawSkeleton(body);
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
         }
 
         private ImageSource ToBitmap(ColorFrame frame)
